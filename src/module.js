ba926aab3cee07a6b66e7c9ea3d6d0abbeec0ea8
xfcf1 <xfcf@163.com>
2016-06-22 10:42:08 +0800

    Update module.js

--- src/module.js
+++ src/module.js
@@ -3,7 +3,9 @@ import ReactDom from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import router from './router';
 
-//import createHashHistory from 'history/lib/createHashHistory';
-//const history = createHashHistory({queryKey: false});
+//import { Router, useRouterHistory } from 'react-router'
+//import { createHashHistory } from 'history'
+// useRouterHistory creates a composable higher-order function
+//const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 ReactDom.render(<Router history={browserHistory}>{router}</Router>, document.getElementById('content'));
