097bafb148bfc9d91d2b27a44a8aa4fc74bddc72
alex <xiaohao.dai@ele.me>
2016-06-24 16:28:47 +0800

    修改配置

--- src/module.js
+++ src/module.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, hashHistory } from 'react-router';
 import router from './router';
 
 //import { Router, useRouterHistory } from 'react-router'
