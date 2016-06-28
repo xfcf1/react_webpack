92ea67d3a111a306b5df6a7c192cd65f8d81d243
alex <xiaohao.dai@ele.me>
2016-06-28 17:28:16 +0800

    修改配置

---
+++ src/ajax.js
@@ -0,0 +1,34 @@
+import 'zepto/zepto.min';
+const domain = 'https://itzzqa.rajax.me';
+import { isLoading, isError } from './actions/modal';
+
+export default function requireData(url, data={}, success, method='POST'){
+    return dispatch => {
+        dispatch(isLoading(true));
+        const token = sessionStorage.getItem('token') || '386c68cb-d097-4fff-bb02-c309c76a3481';
+        data.token = token;
+        data = data ? JSON.stringify(data) : '';
+        Zepto.ajax({
+            url: `${domain}${url}`,
+            headers: {'Content-Type': 'application/json'},
+            type: method,
+            dataType: 'json',
+            data: data,
+            success: ((data) => {
+                dispatch(isLoading(false));
+                if(data.code == 0){
+                    if(typeof success == 'function'){
+                        success(data);
+                    }
+                }
+                else {
+                    dispatch(isError(true, data.msg));
+                }
+            }),
+            error: ((data) => {
+                dispatch(isLoading(false));
+                dispatch(isError(true, data.msg));
+            })
+        })
+    }
+}
