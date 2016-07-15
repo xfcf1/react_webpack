f516dd925692343194411eafdabb14d60b997563
xfcf1 <xfcf@163.com>
2016-07-15 16:55:54 +0800

    Update modal.js

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -1,7 +1,7 @@
 import objectAssign from 'object-assign';
 import { IS_LOADING, IS_ERROR } from '../actions/modal';
 
-export loading(state = {show: false, msg: '正在加载中。。。'}, action){
+export function loading(state = {show: false, msg: '正在加载中。。。'}, action){
     switch (action.type) {
         case IS_LOADING:
             return objectAssign({}, state, {show: action.show, msg: action.msg})
@@ -9,7 +9,7 @@ export loading(state = {show: false, msg: '正在加载中。。。'}, action){
             return state;
     }
 }
-export error(state = {show: false, msg: '出错了'}, action){
+export function error(state = {show: false, msg: '出错了'}, action){
     switch (action.type) {
         case IS_ERROR:
             return objectAssign({}, state, {show: action.show, msg: action.msg})
